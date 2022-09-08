75c75b2b6e548aba84d77ce9a23fdd7ddee509cb
nVITTOadrianto <112715649+nVITTOadrianto@users.noreply.github.com>
2022-09-08 09:27:04 +0700

    Add files via upload

---
+++ pertemuan2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+//	int tahunAngkatan;
+//	string jurusan;
+//	
+//	cout << "Kamu tahun angkatan berapa di UNILA?\n";
+//	cin >> tahunAngkatan;
+//	cin.ignore();
+//	
+//	cout << "\nDan kamu di jurusan mana?\n";
+//	getline(cin,jurusan);
+//	
+//	cout << "\nAnda tahun angkatan " << tahunAngkatan << " dan Anda berada di jurusan " << jurusan;
+//	return 0;
+
+	cout<<"PERHITUNGAN KELILING DAN LUAS PERSEGI\n\n";
+	
+	int s;
+	
+	cout<<"Berapa ukuran sisi persegi?\n";
+	cin>>s;
+	
+	cout<<"\nKeliling = "<<4*s;
+	cout<<"\nLuas = "<<s*s;
+	
+	return 0;
+	
+}
